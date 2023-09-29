b1e871db58d5de268a516876fd3929edc5edda55
david-why <david_why@outlook.com>
2023-09-29 16:38:43 +0800

    add pwa things

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,50 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{css,js,svg,ico,html}'],
+        swDest: 'dist/sw.js'
+      },
+      manifest: {
+        name: 'Book Recommendations',
+        short_name: 'BookRec',
+        description: 'A website that recommends books to students',
+        theme_color: '#424242',
+        background_color: '#ccc',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
